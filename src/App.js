ce7c2481d53f03c1ecedf0b59bcf81154b4f2277
samarth.narasimha <samarth.narasimha@skillassure.com>
2022-10-12 14:50:18 +0530

    Counter App using Redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import { useSelector, useDispatch } from 'react-redux';
 import './App.css';
+import { actions } from './store/index';
 
 function App() {
+  const counter = useSelector((state)=>state.counter)
+  const dispatch = useDispatch();
+  const increment = () => {
+    dispatch(actions.increment());
+  }
+  const decrement = () => {
+    dispatch(actions.decrement());
+  }
+  const rand = () => {
+    dispatch(actions.rand(Math.floor(Math.random()*10)));
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Counter App</h1>
+      <button onClick={increment}>+</button><h2>{counter}</h2><button onClick={decrement}>-</button>
+      <button onClick={rand}>Add Random</button>
     </div>
   );
 }
---
+++ src/store/index.js
@@ -0,0 +1,48 @@
+// import {createStore} from 'redux';
+
+// const reducerFn = (state={counter: 0},action) => {
+
+//     if(action.type==="INC"){
+//         return {counter: state.counter+1};
+//     }
+
+//     if(action.type==="DEC"){
+//         return {counter: state.counter-1};
+//     }
+
+//     if(action.type==="RAND"){
+//         return {counter: state.counter+action.payload};
+//     }
+
+//     return state;
+// };
+
+// const store = createStore(reducerFn)
+// export default store
+
+// This is using Redux ToolKit
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState: {counter: 0},
+    reducers: {
+        increment(state, action) {
+            state.counter ++;
+        },
+        decrement(state, action) { 
+            state.counter --;
+        },
+        rand(state, action) { 
+            state.counter += action.payload;
+        }
+    }
+})
+
+export const actions = counterSlice.actions;
+
+const store = configureStore({
+    reducer: counterSlice.reducer
+})
+
+export default store
